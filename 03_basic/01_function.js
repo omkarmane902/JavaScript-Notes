eea7fe6ccf19e11c180456caee5e005fb6b6a5c9
onkar Mane <157266293+omkarmane902@users.noreply.github.com>
2025-03-19 00:23:51 +0530

    Add files via upload

---
+++ 03_basic/01_function.js
@@ -0,0 +1,22 @@
+//how to call function
+
+function  ex1(){
+    console.log("omkar");
+    console.log("mane");
+      console.log("9617");
+      
+}
+
+// ex1()
+
+
+
+function loginuser (username){
+    if( username===undefined){
+ console.log("please enter you user name");
+ return
+    }
+return `${username} is login`
+
+}
+console.log(loginuser());
